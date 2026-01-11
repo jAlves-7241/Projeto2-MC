c1a2c903f4a200cea9354d488042f81ca093601f
afonso002 <afonsoclara06@gmail.com>
2026-01-11 22:14:10 +0000

    Add files via upload

--- pergunta1.m
+++ pergunta1.m
@@ -2,20 +2,18 @@ clc, clearvars;
 
 TolS = 1e-3;
 MaxIter = 20;
-g = @(x) x.^20 .* exp(x-1); % Integranda
+g = @(x) x.^20 .* exp(x-1);
 
-% Exato 
-vExato = integral(g, 0, 1)
+vExato = integral(g, 0, 1) % Exato
 
-% Simpson
-[vSimpson, ~, ~, ~, ~] = simpsonmetodo(g, 0, 1, TolS, MaxIter)
+[vSimpson, ~, ~, ~, ~] = simpsonmetodo(g, 0, 1, TolS, MaxIter) % Simpson
 
 % Recorrência
 i(1) = exp(-1);
 for k = 2:20
     i(k) = 1 - k*i(k-1);  
 end
-vRec = i(20) % Fórmula instável, provavelmente o objetivo para o relatório
+vRec = i(20)
 
 % Comparação
 eAbsSimpson = abs(vExato - vSimpson)
