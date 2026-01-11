077c631f91bb3baf15a826fa35a82580a695ed04
jAlves <jpapsalves@gmail.com>
2026-01-11 12:51:11 +0000

    Add files via upload

--- compritraj.m
+++ compritraj.m
@@ -1,16 +1,22 @@
-function [l, pontos] = compritraj(f, intx, intu, t0, tn, n)
+function [c, pontos] = compritraj(f, intx, intu, t0, tn, n)
 
-    numGrid = 101;
+    NumGrid = 101;
+    c = zeros(1, NumGrid^2); % Pré-alocar
+    pontos = zeros(NumGrid^2, 2); % Pré-alocar
 
-    x = linspace(intx(1), intx(2), numGrid);
-    v = linspace(intu(1), intu(2), numGrid);
+    if rem(n,2) == 1
+      disp('Number of intervals must be even');
+      return;
+    end
+
+    x = linspace(intx(1), intx(2), NumGrid);
+    v = linspace(intu(1), intu(2), NumGrid);
     [xx, vv] = meshgrid(x, v);
     pontos = [xx(:), vv(:)];
     
     dudt = zeros(2, n+1); % Pré-alocar
-    l = zeros(1, numGrid^2); % Pré-alocar
 
-    for i = 1:numGrid^2
+    for i = 1:NumGrid^2
 
         [~, sol, t, ~] = rk4rn(f, pontos(i,:), t0, tn, n);
         
@@ -19,6 +25,6 @@ function [l, pontos] = compritraj(f, intx, intu, t0, tn, n)
         end
 
         vNorm = vecnorm(dudt, 2, 1);
-        l(i) = simpson(t, vNorm);    
+        c(i) = simpson(t, vNorm);    
         
     end
--- prob2d.m
+++ prob2d.m
@@ -2,8 +2,8 @@ clc, clearvars
 
 vanDerPol = @(t, y) [y(2); 4.*(1-y(1).^2).*y(2) - y(1)];
 
-[l, pontos] = compritraj(vanDerPol, [-3,3], [-8,8], 0, 4, 100);
+[c, pontos] = compritraj(vanDerPol, [-3,3], [-8,8], 0, 4, 100);
 
-plot3(pontos(:,1), pontos(:,2), l)
+plot3(pontos(:,1), pontos(:,2), c)
 title('Distância percorrida em função das condições iniciais')
 xlabel('x_0 (m)'), ylabel("x'_0 (m/s)"), zlabel('d (m)')
