8bede50f79406bade961c9b32bb2708e9a876eb2
jAlves <jpapsalves@gmail.com>
2026-01-11 13:29:31 +0000

    Colormap

---
+++ extraColormap.m
@@ -0,0 +1,22 @@
+% Script utilizado para gerar o colormap do problema 2d)
+clc, clearvars, clf
+
+vanDerPol = @(t, y) [y(2); 4.*(1-y(1).^2).*y(2) - y(1)];
+
+[c, pontos] = compritraj(vanDerPol, [-3,3], [-8,8], 0, 4, 100);
+
+%% Colormap
+numGrid = sqrt(length(c)); % Dá NumGrid
+
+% Reorganizar em matrizes quadradas para mesh
+X = reshape(pontos(:,1), numGrid, numGrid);
+Y = reshape(pontos(:,2), numGrid, numGrid);
+Z = reshape(c, numGrid, numGrid);
+
+mesh(X, Y, Z);
+
+shading interp; % Suaviza as cores consoante a altura
+colormap jet; colorbar;
+
+title('Distância percorrida em função das condições iniciais');
+xlabel('x_0 (m)'), ylabel("x'_0 (m/s)"), zlabel('d (m)'),
